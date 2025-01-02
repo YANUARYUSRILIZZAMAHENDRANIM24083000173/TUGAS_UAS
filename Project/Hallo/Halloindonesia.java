83325fefed6dbe8de54add2749a68fb6765faed8
ZHAZHA <mryanuar6525@gmail.com>
2025-01-03 00:01:13 +0700

    Create Halloindonesia.java

---
+++ Project/Hallo/Halloindonesia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Hallo;
+
+/**
+ *
+ * @author sanda
+ */
+public class Halloindonesia {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo indonesia");
+        System.out.println("Selamat pagi indonesia");
+        System.out.println("Selamat siang indonesia");
+        System.out.println("Selamat malam indonesia");
+      
+    }
+}

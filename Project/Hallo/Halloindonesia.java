9195e2773cccab74192e2e0d98475f50013f5522
ZHAZHA <mryanuar6525@gmail.com>
2025-01-03 00:32:38 +0700

    Update Halloindonesia.java

--- Project/Hallo/Halloindonesia.java
+++ Project/Hallo/Halloindonesia.java
@@ -13,8 +13,6 @@ public class Halloindonesia {
     public static void main(String[] args) {
         System.out.println("Hallo indonesia");
         System.out.println("Selamat pagi indonesia");
-        System.out.println("Selamat siang indonesia");
-        System.out.println("Selamat malam indonesia");
-      
+        System.out.println("Jaya indonesia");
     }
 }
